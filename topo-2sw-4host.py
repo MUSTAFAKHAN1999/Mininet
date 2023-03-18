69e8021dafca15713629fc7424e75813ba1d679f
MUSTAFAKHAN1999 <113144060+MUSTAFAKHAN1999@users.noreply.github.com>
2023-03-18 15:16:02 -0500

    Add files via upload

---
+++ topo-2sw-4host.py
@@ -0,0 +1,36 @@
+"""Custom topology example
+
+Two directly connected switches plus a host for each switch:
+
+   host --- switch --- switch --- host
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def build( self ):
+        "Create custom topo."
+
+        # Add hosts and switches
+        Host1 = self.addHost( 'h1' )
+        Host2 = self.addHost( 'h2' )
+        Host3 = self.addHost( 'h3' )
+        Host4 = self.addHost( 'h4' )
+        Switch1 = self.addSwitch( 's1' )
+        Switch2 = self.addSwitch( 's2' )
+
+        # Add links
+        self.addLink( Host1, Switch1 )
+        self.addLink( Host2, Switch1 )
+        self.addLink( Host3, Switch2 )
+        self.addLink( Host4, Switch2 )
+        self.addLink( Switch1, Switch2 )
+
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
